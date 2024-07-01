1338b80d49fa6586a337fff000331110b3bcb170
Stefin George <georgestefin24@outlook.com>
2024-07-01 19:26:58 +0530

    Printing elements of array of list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,11 @@
 function ListGroup() {
+  const items = ["Mumbai", "Chennai", "Kolkata", "Bangalore", "Delhi"];
   return (
     <>
       <h1>List</h1>
-      <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
-      </ul>
+      {items.map((item) => (
+        <li>{item}</li>
+      ))}
     </>
   );
 }
